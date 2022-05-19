cef57c64856ec6404a44a7995e0074bb0da7818c
Mingyou Sung <mingyousung@gmail.com>
2022-05-19 13:31:09 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import torch
 import argparse
 from text_processing import get_nlp_pipeline, word_tokenization
 import my_onehot
-from attentions import ScaledDotProductAttention, MultiHeadAttention
+from attentions import ScaledDotProductAttention, MultiHeadAttention, AbsolutePositionalEncoder
 
 def main(args):
     text_list = ["We are about to study the idea of a computational process.", 
@@ -16,18 +16,18 @@ def main(args):
 
     
     # One-hot Encoding
-    sklearn_onehotencoder = my_onehot.build_onehot_encoding_model(args.unk_ignore)
-    token2idx_dict, _ = my_onehot.init_token2idx(text_list, args.nlp_pipeline)
-    sklearn_onehotencoder.fit([[t] for t in token2idx_dict])
-    tks = my_onehot.get_tokens(cur_text, args.nlp_pipeline)
+    embeddings = my_onehot.get_onehot_encoding(text_list, cur_text, args.nlp_pipeline, args.unk_ignore)
 
-    embeddings = my_onehot.onehot_encoding(sklearn_onehotencoder, tks)
-    print("Raw Sklearn One-hot Encoding Result")
-    print(embeddings)
-    
+
     batch_size = 1
     input_length, emb_dim = embeddings.size()
-    query = key = value = embeddings.reshape(batch_size, input_length, emb_dim)
+
+    input_embedding = embeddings.reshape(batch_size, input_length, emb_dim)
+
+    # input embedding + positional encoding
+    positional_encoder = AbsolutePositionalEncoder(emb_dim)
+    input_embedding = input_embedding + positional_encoder(input_embedding)
+    query = key = value = input_embedding
 
     if args.attention == "scaleddotproduct":
         model = ScaledDotProductAttention(emb_dim)
