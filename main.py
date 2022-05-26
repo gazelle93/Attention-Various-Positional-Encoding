7d953cace1e4b4be0115c47332fdabec6432fcd7
Mingyou Sung <mingyousung@gmail.com>
2022-05-26 09:36:02 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import torch
 import argparse
 from text_processing import get_nlp_pipeline, word_tokenization
 import my_onehot
-from attentions import ScaledDotProductAttention, MultiHeadAttention, AbsolutePositionalEncoder
+from attentions import AbsolutePositionalEncoder, RelativePositionalEncoder, T5RelativePositionalEncoder
+from attentions import ScaledDotProductAttention, RelativeScaledDotProductAttention, T5ScaledDotProductAttention
+from attentions import MultiHeadAttention
 
 def main(args):
     text_list = ["We are about to study the idea of a computational process.", 
@@ -29,43 +31,119 @@ def main(args):
     input_embedding = input_embedding + positional_encoder(input_embedding)
     query = key = value = input_embedding
 
-    if args.attention == "scaleddotproduct":
-        model = ScaledDotProductAttention(emb_dim)
-        output, attn_score = model(query, key, value)
-        
-        print("Scaled-Dot-Product Attention Result")
-        print(output)
-        
-    elif args.attention == "multihead":
-        if emb_dim % args.num_heads != 0:
-            divisor_list = []
-            for i in range(1, emb_dim):
-                if emb_dim % i == 0:
-                    divisor_list.append(i)
-            num_heads = divisor_list[len(divisor_list)//2]
-        else:
-            num_heads = args.num_heads
+    seq_len_query = query.size()[1]
+    seq_len_key = key.size()[1]
 
-        model = MultiHeadAttention(emb_dim, num_heads)
-        output, attn_score = model(query, key, value)
+    # Absolute Positional Encoding
+    if args.positional_encoding == "abs":
+        if args.attention == "scaleddotproduct":
+            model = ScaledDotProductAttention(emb_dim)
+            output, attn_score = model(query, key, value)
 
-        print("Multi-Head Attention Result (Number of heads: {})".format(num_heads))
-        print(output)
-      
-    else:
-        print("Not seleceted.")
+            print("Scaled-Dot-Product Attention Result")
+            print(output)
+
+        elif args.attention == "multihead":
+            if emb_dim % args.num_heads != 0:
+                divisor_list = []
+                for i in range(1, emb_dim):
+                    if emb_dim % i == 0:
+                        divisor_list.append(i)
+                num_heads = divisor_list[len(divisor_list)//2]
+            else:
+                num_heads = args.num_heads
+
+            model = MultiHeadAttention(emb_dim, num_heads)
+            output, attn_score = model(query, key, value)
+
+            print("Multi-Head Attention Result (Number of heads: {})".format(num_heads))
+            print(output)
+
+
+    # Relative Positional Encoding
+    elif args.positional_encoding == "rel":
+        if args.attention == "scaleddotproduct":
+            relative_position_k = RelativePositionalEncoder(emb_dim)
+            relative_position_v = RelativePositionalEncoder(emb_dim)
+
+            seq_len_query = query.size()[1]
+            seq_len_key = key.size()[1]
+            seq_len_value = value.size()[1]
+
+            a_key = relative_position_k(seq_len_query, seq_len_key)
+            a_value = relative_position_v(seq_len_query, seq_len_value)
+
+            model = RelativeScaledDotProductAttention(emb_dim)
+
+            output, attn_score = model(query, key, value, a_key, a_value)
+
+            print("Relative Scaled-Dot-Product Attention Result")
+            print(output)
+
+
+        elif args.attention == "multihead":
+            if emb_dim % args.num_heads != 0:
+                divisor_list = []
+                for i in range(1, emb_dim):
+                    if emb_dim % i == 0:
+                        divisor_list.append(i)
+                num_heads = divisor_list[len(divisor_list)//2]
+            else:
+                num_heads = args.num_heads
+
+            model = MultiHeadAttention(emb_dim, num_heads)
+            output, attn_score = model(query, key, value)
+
+            print("Relative Multi-Head Attention Result (Number of heads: {})".format(num_heads))
+            print(output)
+
+
+    # T5 Relative Positional Encoding
+    elif args.positional_encoding == "t5":
+        if args.attention == "scaleddotproduct":
+            relative_position_bias = T5RelativePositionalEncoder(1)
+
+            seq_len_query = query.size()[1]
+            seq_len_key = key.size()[1]
+
+            relative_bias = relative_position_bias(seq_len_query, seq_len_key)
+            print(relative_bias.size())
+            print("-----")
+
+            model = T5ScaledDotProductAttention(emb_dim)
+
+            output, attn_score = model(query, key, value, relative_bias)
+
+            print("Relative Scaled-Dot-Product Attention Result")
+            print(output)
+
+
+        elif args.attention == "multihead":
+            if emb_dim % args.num_heads != 0:
+                divisor_list = []
+                for i in range(1, emb_dim):
+                    if emb_dim % i == 0:
+                        divisor_list.append(i)
+                num_heads = divisor_list[len(divisor_list)//2]
+            else:
+                num_heads = args.num_heads
+
+            model = MultiHeadAttention(emb_dim, num_heads)
+            output, attn_score = model(query, key, value)
+
+            print("Relative Multi-Head Attention Result (Number of heads: {})".format(num_heads))
+            print(output)
     
     
 
-          
-    
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     parser.add_argument("--nlp_pipeline", default="spacy", type=str, help="NLP preprocessing pipeline.")
     parser.add_argument("--unk_ignore", default=True, help="Ignore unknown tokens.")
     parser.add_argument("--num_heads", default=8, help="The number of heads for multi-head attention.")
-    parser.add_argument("--attention", default="multihead", type=str, help="Type of attention layer. (scaleddotproduct, multihead)")
+    parser.add_argument("--attention", default="scaleddotproduct", type=str, help="Type of attention layer. (scaleddotproduct, multihead)")
+    parser.add_argument("--positional_encoding", default="abs", type=str, help="Type of positional encoding. (abs, rel, t5)")
     args = parser.parse_args()
 
     main(args)
